493728fef9d91839e0a15a77b83b9148c3a85017
albert-611 <albertprogramming611@gmail.com>
2026-03-25 00:06:14 +0600

    js

---
+++ 07_concatenateString.js
@@ -0,0 +1,12 @@
+var myStr = "This is the start. " + "This is the end."
+console.log(myStr);
+
+//concatenating Strings with Plus Equals Operator
+var ourStr = "I come First. ";
+ourStr += "I come Second.";
+
+var first = "Albert ";
+var second = "Smith ";
+var  third = "Bala";
+
+console.log(first+second+third);
