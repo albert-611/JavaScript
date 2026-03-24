863114406bb63bdddc1c382c3400111565c02ad1
albert-611 <albertprogramming611@gmail.com>
2026-03-25 00:05:33 +0600

    js

---
+++ 03_dataTypeandVariables.js
@@ -0,0 +1,11 @@
+/* Data Types:
+undefined,null,boolen,string,symbol.number, and object
+*/
+
+var myname = "Smith" // used in whole program
+
+myname = 8
+
+let ourName = "Albert" // where it declare only
+
+const pi = 3.14 //can never change
