e23a676be64dedd2f17e7c4d6b4f9a18a34e41ea
albert-611 <albertprogramming611@gmail.com>
2026-03-25 00:05:50 +0600

    js

---
+++ 05_arrithmaticOperator.js
@@ -0,0 +1,52 @@
+var a = 5,b = 10,c = "I am a ";
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+console.log(a,b);
+console.log(c);
+
+var sum = 10 + 10;
+console.log(sum)
+
+var subtraction = 170 - 12;
+console.log("Subtraction = ", subtraction);
+
+var product = 79 * 2;
+console.log("Multiplication = ",product);
+
+var quotient = 66 / 33;
+console.log("Divition = ", quotient);
+
+var increment = 157;
+increment++;
+console.log(increment);
+
+var decrement = 159;
+decrement--;
+console.log(decrement);
+
+var decimal = 5.7
+console.log(decimal);
+
+var deciProduct = 2.3 * 2.5
+console.log(deciProduct);
+
+var deciQuotient = 4.4 / 2.0
+console.log(deciQuotient);
+
+var remainder = 11 % 3;
+console.log(remainder);
+
+var num1 = 3, num2 = 17, num3 = 12;
+
+num1 += 12;
+num2 -= 9;
+num3 *= 7;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+num3 /= num2;
+console.log(num3);
+
